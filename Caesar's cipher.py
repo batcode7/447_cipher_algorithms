19d99932e0bd28e41b562a5b9404a29476fb4ed0
batcode7 <partha.bhoumik@g.bracu.ac.bd>
2022-09-30 01:44:09 +0600

    Caesar's cipher updated(removed list) cipher with key

--- Caesar's cipher.py
+++ Caesar's cipher.py
@@ -1,24 +1,11 @@
 checker = input ("Please enter 1 to encrypt and 2 to decrypt ")
+cipher_text = input("Please enter the Cipher text: ")
 if checker == 1:
-  """
-  Caesar's cipher
-  (encoder)
-  """
-  cipher_text = input("Please enter the Cipher text: ")
-  cipher_list = []
+  """ Caesar's cipher(encoder) """
   for i in cipher_text:
-    cipher_list.append(chr(ord(i) + 3))
-  print(cipher_list)
-
+    print(chr(ord(i) + 3), end=" ")
 else:
-  """
-  Caesar's cipher
-  (decoder)
-  """
-
-  cipher_text = input("Please enter the Cipher text: ")
-  cipher_list = []
+  """ Caesar's cipher(decoder) """
   for i in cipher_text:
-    cipher_list.append(chr(ord(i) - 3))
-  print(cipher_list)
+    print(chr(ord(i) - 3), end=" ")
 
--- N substituition cipher with key.py
+++ N substituition cipher with key.py
@@ -1,21 +1,11 @@
-"""
-Caesar's cipher
-(encoder)
-"""
-
-cipher_text = input ("Please enter the Cipher text: ")
-cipher_list = []
-for i in cipher_text:
-  cipher_list.append(chr(ord(i) + 3))
-print (cipher_list)
-
-"""
-Caesar's cipher
-(decoder)
-"""
-
-cipher_text = input ("Please enter the Cipher text: ")
-cipher_list = []
-for i in cipher_text:
-  cipher_list.append(chr(ord(i) - 3))
-print (cipher_list)
+checker = input("Please enter 1 to encrypt and 2 to decrypt ")
+cipher_text = input("Please enter the Cipher text: ")
+key = int(input("Please enter the key: "))
+if checker == 1:
+  """ Caesar's cipher(encoder) """
+  for i in cipher_text:
+    print(chr(ord(i) + key), end=" ")
+else:
+  """ Caesar's cipher(decoder) """
+  for i in cipher_text:
+    print(chr(ord(i) - key), end=" ")
