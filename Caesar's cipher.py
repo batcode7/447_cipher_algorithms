fbec5710807df77dc1050a2db4e046bc4687fa35
batcode7 <partha.bhoumik@g.bracu.ac.bd>
2022-09-30 01:09:47 +0600

    Caesar's cipher update

--- Caesar's cipher.py
+++ Caesar's cipher.py
@@ -1,21 +1,24 @@
-"""
-Caesar's cipher
-(encoder)
-"""
+checker = input ("Please enter 1 to encrypt and 2 to decrypt ")
+if checker == 1:
+  """
+  Caesar's cipher
+  (encoder)
+  """
+  cipher_text = input("Please enter the Cipher text: ")
+  cipher_list = []
+  for i in cipher_text:
+    cipher_list.append(chr(ord(i) + 3))
+  print(cipher_list)
 
-cipher_text = input ("Please enter the Cipher text: ")
-cipher_list = []
-for i in cipher_text:
-  cipher_list.append(chr(ord(i) + 3))
-print (cipher_list)
+else:
+  """
+  Caesar's cipher
+  (decoder)
+  """
 
-"""
-Caesar's cipher
-(decoder)
-"""
+  cipher_text = input("Please enter the Cipher text: ")
+  cipher_list = []
+  for i in cipher_text:
+    cipher_list.append(chr(ord(i) - 3))
+  print(cipher_list)
 
-cipher_text = input ("Please enter the Cipher text: ")
-cipher_list = []
-for i in cipher_text:
-  cipher_list.append(chr(ord(i) - 3))
-print (cipher_list)
