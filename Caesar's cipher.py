40de368ac5a6ac00ce691c78a373fd9103912c46
batcode7 <partha.bhoumik@g.bracu.ac.bd>
2022-10-01 01:19:14 +0600

    added double transposition cipher v1

--- Caesar's cipher.py
+++ Caesar's cipher.py
@@ -1,6 +1,6 @@
 checker = input ("Please enter 1 to encrypt and 2 to decrypt ")
 cipher_text = input("Please enter the Cipher text: ")
-if checker == 1:
+if checker == "1":
   """ Caesar's cipher(encoder) """
   for i in cipher_text:
     print(chr(ord(i) + 3), end=" ")
